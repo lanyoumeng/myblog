283f629cdb9d031bcb54cda35c5bed82afec891b
lanmengyou <2058818914@qq.com>
2023-08-04 01:19:29 +0800

    feat:第二次提交

--- cmd/myblog/main.go
+++ cmd/myblog/main.go
@@ -5,22 +5,9 @@
 
 package main
 
-import (
-	"net/http"
+import "fmt"
 
-	"github.com/gin-gonic/gin"
-)
-
+// Go 程序的默认入口函数(主函数).
 func main() {
-
-	r := gin.Default()
-	r.GET("/data", func(c *gin.Context) {
-		data := gin.H{
-			"key1": "value1",
-			"key2": 123,
-		}
-		c.JSON(http.StatusOK, data)
-	})
-	r.Run(":8080")
-
+	fmt.Println("Hello  MiniBlog!")
 }
